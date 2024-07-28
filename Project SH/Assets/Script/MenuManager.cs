14af522532d867d41b960f0e8814b287f221e543
pjnaden2k12 <115402754+pjnaden2k12@users.noreply.github.com>
2024-07-29 04:22:27 +0700

    update

---
+++ Project SH/Assets/Script/MenuManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public Button shopButton; // Nút Shop
+    public Button equipmentButton; // Nút Equipment
+    public Button battleButton; // Nút Battle
+    public Button towerButton; // Nút Tower
+    public Button leaderboardButton; // Nút Leaderboard
+
+    void Start()
+    {
+        // Gán sự kiện cho các nút
+      
+        shopButton.onClick.AddListener(OpenShop);
+        equipmentButton.onClick.AddListener(OpenEquipment);
+        battleButton.onClick.AddListener(OpenBattle);
+        towerButton.onClick.AddListener(OpenTower);
+        leaderboardButton.onClick.AddListener(OpenLeaderboard);
+    }
+
+
+    void OpenShop()
+    {
+        // Mở màn hình Shop
+        SceneManager.LoadScene("ShopScene"); // Thay "ShopScene" bằng tên scene của Shop
+    }
+
+    void OpenEquipment()
+    {
+        // Mở màn hình Equipment
+        SceneManager.LoadScene("EquipmentScene"); // Thay "EquipmentScene" bằng tên scene của Equipment
+    }
+
+    void OpenBattle()
+    {
+        
+        SceneManager.LoadScene("MapScene"); 
+    }
+
+    void OpenTower()
+    {
+        // Mở màn hình Tower
+        SceneManager.LoadScene("TowerScene"); // Thay "TowerScene" bằng tên scene của Tower
+    }
+
+    void OpenLeaderboard()
+    {
+        // Mở màn hình Leaderboard
+        SceneManager.LoadScene("LeaderboardScene"); // Thay "LeaderboardScene" bằng tên scene của Leaderboard
+    }
+}
---
+++ Project SH/Assets/Script/video.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer; // Video Player component
+
+    void Start()
+    {
+        if (videoPlayer != null)
+        {
+            videoPlayer.Play(); // Bắt đầu phát video khi bắt đầu trò chơi
+        }
+    }
+}
