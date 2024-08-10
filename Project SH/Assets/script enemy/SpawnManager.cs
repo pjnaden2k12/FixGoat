3402c58f90f1645bb99dff4e3292cb0dca7b54da
XuanTung05 <trinhxuantung05@gmail.com>
2024-08-10 22:03:55 +0700

    update

--- Project SH/Assets/script enemy/SpawnManager.cs
+++ Project SH/Assets/script enemy/SpawnManager.cs
@@ -48,7 +48,7 @@ public class SpawnManager : MonoBehaviour
         int randomTargetIndex = Random.Range(0, targetPoints.Length);
         Transform targetPoint = targetPoints[randomTargetIndex];
 
-        BGoblinHealth enemyHealth = enemy.GetComponent<BGoblinHealth>();
+        EnemyHealth enemyHealth = enemy.GetComponent<EnemyHealth>();
         if (enemyHealth != null)
         {
             enemyHealth.targetPoint = targetPoint;
